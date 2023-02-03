58f58484a42373de2ff2144f92e9f27f179ee961
John Mabanta <johnmabanta51@gmail.com>
2023-02-02 17:58:14 -0700

    Added shape class

--- app/src/main/java/com/example/cmput301w23t09/Shape.java
+++ app/src/main/java/com/example/cmput301w23t09/Shape.java
@@ -1,3 +1,32 @@
 package com.example.cmput301w23t09;
 
-// TODO: As a team
+public abstract class Shape {
+
+    protected int x, y;
+
+    public Shape() {
+        x = 0;
+        y = 0;
+    }
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
