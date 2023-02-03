7255c73ce419a2b464779af6fbbc6f3697166566
WillQi <williamqipizza@gmail.com>
2023-02-02 18:01:17 -0700

    adds circle

---
+++ app/src/main/java/com/example/cmput301w23t09/Circle.java
@@ -0,0 +1,13 @@
+package com.example.cmput301w23t09;
+
+public class Circle extends Shape {
+
+    public Circle() {
+        super();
+    }
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+
+}
