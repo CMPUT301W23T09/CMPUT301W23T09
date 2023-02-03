31b8082ebfa83da631a2a975c077869bb14c0ed6
WillQi <williamqipizza@gmail.com>
2023-02-02 18:08:56 -0700

    add radius to circle class

--- app/src/main/java/com/example/cmput301w23t09/Circle.java
+++ app/src/main/java/com/example/cmput301w23t09/Circle.java
@@ -2,12 +2,27 @@ package com.example.cmput301w23t09;
 
 public class Circle extends Shape {
 
-    public Circle() {
+    protected float radius;
+
+    public Circle(float radius) {
         super();
+        this.setRadius(radius);
     }
 
-    public Circle(int x, int y) {
+    public Circle(int x, int y, float radius) {
         super(x, y);
+        this.setRadius(radius);
+    }
+
+    public float getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(float radius) {
+        if (radius < 0) {
+            throw new IllegalArgumentException("Please specify a radius above 0.");
+        }
+        this.radius = radius;
     }
 
 }
